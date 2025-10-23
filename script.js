41bdbe69880e045f72b41a085e1d2cd99ea2c898
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-23 10:48:47 +0000

    Add variables to querySelect display and buttons

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const display = document.querySelector("#display");
+const dot = document.querySelector("#point");
+const equals = document.querySelector("#calculate");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#del");
+const nums = [
+    document.querySelector("#num0"),
+    document.querySelector("#num1"),
+    document.querySelector("#num2"),
+    document.querySelector("#num3"),
+    document.querySelector("#num4"),
+    document.querySelector("#num5"),
+    document.querySelector("#num6"),
+    document.querySelector("#num7"),
+    document.querySelector("#num8"),
+    document.querySelector("#num9"),
+];
+
+const opCodes = {
+    add: document.querySelector("#opAdd"),
+    sub: document.querySelector("#opSub"),
+    mul: document.querySelector("#opMul"),
+    div: document.querySelector("#opDiv"),
+};
+
 function add(a, b) {
     return +a + +b;
 }
