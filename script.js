df54e7e8c40f9de17bca49c5e244cc0849fdb443
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-23 10:17:53 +0000

    Add operate() to call the necessary functions

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function divide(a, b) {
     }
     return;
 }
+
+/**
+ * 
+ * @param {String} operator The math operation to be used
+ * @param {Number} op1 The first operand
+ * @param {Number} op2 The second operand
+ */
+function operate(operator, op1, op2) {
+   switch(operator) {
+    case "+":
+        add(op1, op2);
+        break;
+    case "-":
+        subtract(op1, op2);
+        break;
+    case "*":
+        multiply(op1, op2);
+        break;
+    case "/":
+        divide(op1, op2);
+        break;
+   } 
+}
